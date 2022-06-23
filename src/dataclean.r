5bec925f2375726863f3244ec7e5ea424a8fdaac
Lucasodn <106759753+Lucasodn@users.noreply.github.com>
2022-06-23 12:50:19 +0200

    cleaned data to produce main output

--- src/dataclean.r
+++ src/dataclean.r
@@ -9,17 +9,13 @@ turnen <- read.csv("../data/Proxy Nationalism/Turnvereine Prussia.csv", sep = ";
 colnames(turnen)
 turn <- turnen %>% dplyr::select(city, Jahr, Gesamt, Beruf_Handwerk, Beruf_Gelehrte, Beruf_Kaufleute)
 
-head(turn)
-# selecting for city, year, popcivil
-## head(hornung_dt_ps)
-# select only the years relevant, so year 1842
+turn[turn == "na"] <- "Missing Value"
+
+
 
 dat <- hornung_dt_ps %>%
-  dplyr::select(city, year, popcivil, kreiskey1849) %>%
-  dplyr::filter(year %in% c(1864, 1843)) %>%
-  group_by(city, kreiskey1849) %>%
-  summarise(delta = (popcivil / lag(popcivil) - 1)) %>%
-  filter(!is.na(delta))
+  dplyr::select(townkey1849, city, year, popcivil, kreiskey1849) %>%
+  dplyr::filter(year %in% c(1864, 1843))
 
 
 match_checker <- function(a, b) {
@@ -59,19 +55,65 @@ sum(matches$is_matched)
 print("cases remaining:")
 sum(!matches$is_matched)
 
-write.csv(dplyr::filter(matches, is_matched == FALSE), "../data/Proxy Nationalism/matches_reference.csv", sep = ";")
+write.csv(dplyr::filter(matches,
+                        is_matched == FALSE),
+          "../data/Proxy Nationalism/matches_reference.csv",
+          sep = ";")
 
 # The issue here is the matching is not working currently. it seems ot be an issue with the CSV encoding.
 
 
-matchmaker <- read_csv("../data/Proxy Nationalism/name_matches.csv")
+matchmaker <- read.csv("../data/Proxy Nationalism/name_matches.csv", sep=";")
 
 matchmaker$Old
 
-for (i in 1:length(matchmaker)) {
+
+for (i in 1:length(matchmaker$Old)) {
   turn$city <- gsub(matchmaker$Old[i], matchmaker$New[i], turn$city)
 }
 
-turn$city <- gsub("Preussisch-Eylau", "Pr. Eylau", turn$city)
+turn <- turn %>% filter(!(city == ""))
 
-turn %>% filter(is.na(city))
+#Drop Variable Jahr (Gründungsjahr Verein)
+turn <- turn %>% select(!Jahr)
+dat <- dat %>% select(!Jahr)
+
+
+
+# Merge Turner to Hornung Cities
+maindf <- left_join(dat, turn, by = "city")
+
+# replace NAs with 0s, as missing values mean no turnverien present.
+maindf$Gesamt <- ifelse(is.na(maindf$Gesamt), 0, maindf$Gesamt)
+maindf$Beruf_Handwerk <- ifelse(is.na(maindf$Beruf_Handwerk), 0, maindf$Beruf_Handwerk)
+maindf$Beruf_Gelehrte <- ifelse(is.na(maindf$Beruf_Gelehrte), 0, maindf$Beruf_Gelehrte)
+maindf$Beruf_Kaufleute <- ifelse(is.na(maindf$Beruf_Kaufleute), 0, maindf$Beruf_Kaufleute)
+
+
+maindf[maindf == "Missing Value"] <- NA
+
+
+maindf <- maindf %>% mutate(
+  Gesamt = as.integer(Gesamt),
+  Beruf_Handwerk = as.integer(Beruf_Handwerk),
+  Beruf_Gelehrte = as.integer(Beruf_Gelehrte),
+  Beruf_Kaufleute = as.integer(Beruf_Kaufleute))
+
+
+# We want to sum "Gesamt" Turner in same city and calculate Turner share on population "popcivil"
+
+maindf <- as_tibble(maindf)
+
+# Processing maindf to summarize total number of turner per town, not per turnverein.
+# the 64 at the end of the variable name is designed to highlight the fact that the number of
+# turner is only for the year 1864
+maindf <- maindf %>%
+  group_by(townkey1849, city, year, kreiskey1849) %>%
+  summarise(popcivil = mean(popcivil),
+            Gesamt64 = sum(Gesamt),
+            Beruf_Handwerk_64 = sum(Beruf_Handwerk),
+            Beruf_Gelehrte_64 = sum(Beruf_Gelehrte),
+            Beruf_Kaufleute_64 = sum(Beruf_Kaufleute))
+  
+
+
